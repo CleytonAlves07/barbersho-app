0ddeeda19547cbb0d38fc321b2e1a3f2b38ce503
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-04-08 13:19:04 -0300

    refactor: :recycle: Alterações necessárias para incluir about, agenda e agendamento no front

--- barbershop-client/src/app/app.routes.ts
+++ barbershop-client/src/app/app.routes.ts
@@ -1,10 +1,26 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AgendaComponent } from './components/agenda/agenda.component';
+import { AgendamentoComponent } from './components/agendamento/agendamento.component';
+import { AboutComponent } from './pages/about/about.component';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'agenda',
+    component: AgendaComponent
+  },
+  {
+    path: 'agendamento',
+    component: AgendamentoComponent
+  },
+  {
+    path: 'sobre',
+    component: AboutComponent
   },
   {
     path: '**',
