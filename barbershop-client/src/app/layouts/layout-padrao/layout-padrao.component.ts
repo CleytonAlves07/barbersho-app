526ce232602056db3a9f0b571eb1a3824bf19a96
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-04-09 15:17:19 -0300

    feat: :sparkles: Separação dos layouts para inclusão de login e register sem navbar e footer

---
+++ barbershop-client/src/app/layouts/layout-padrao/layout-padrao.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-layout-padrao',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
+  `
+})
+export class LayoutPadraoComponent { }
