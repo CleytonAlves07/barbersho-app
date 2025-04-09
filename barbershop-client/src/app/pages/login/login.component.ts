7d77d208b76d66c0fefc9fa18beede382ab51d66
Cleyton Alves <cleyton.alves.a@gmail.com>
2025-04-09 15:18:14 -0300

    feat: :sparkles: Estrutura de login montada

---
+++ barbershop-client/src/app/pages/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink, RouterLinkActive],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
+    this.form = this.fb.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    this.auth.login(this.form.value).subscribe({
+      next: () => this.router.navigate(['/agenda']),
+      error: () => alert('Login inválido')
+    });
+  }
+}
